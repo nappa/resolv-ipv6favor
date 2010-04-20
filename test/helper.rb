a999b800cd8525bcbd10893633aba7e644a51b8d
Ryosuke Yamazaki <ryosuke.yamazaki@mac.com>
2010-04-21 02:57:47 +0900

    Initial commit to resolv-ipv6favor.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'resolv-ipv6favor'
+
+class Test::Unit::TestCase
+end
